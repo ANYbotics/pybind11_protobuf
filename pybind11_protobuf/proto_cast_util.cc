0ce60e8ae8c97a743cb6bcfc5cb1bd3fab63ea76
Javier De Velasco Oriol <jvelasco@anybotics.com>
2024-01-11 13:27:22 +0100

    Adjust some missing includes

--- pybind11_protobuf/proto_cast_util.cc
+++ pybind11_protobuf/proto_cast_util.cc
@@ -15,9 +15,9 @@
 #include "google/protobuf/descriptor.pb.h"
 #include "google/protobuf/descriptor.h"
 #include "google/protobuf/dynamic_message.h"
+#include "google/protobuf/message.h"
 #include "proto_api.h"
-#include "pybind11_protobuf/check_unknown_fields.h"
-
+#include "absl/container/flat_hash_map.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_replace.h"
 #include "absl/strings/str_split.h"
